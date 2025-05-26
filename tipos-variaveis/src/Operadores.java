648a01943d7b0be8a0ff6fc519570423a4bd27fc
Rosevalda <rosevald@hotmail.com>
2025-05-25 22:59:42 -0300

    Meu projeto conta banco

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,9 +1,5 @@
 public class Operadores {
     public static void main(String[] args) {
-       String nomeUm = "Rosevalda";
-       String nomeDois = "Rosevalda";
-
-       System.out.println(nomeUm == nomeDois);
 
     }
 }
